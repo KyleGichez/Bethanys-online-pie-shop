25e08bbe7ac22e82dbd97ff4ae7b97c24b85aceb
Gichure Maina <76261570+KyleGichez@users.noreply.github.com>
2021-05-17 23:55:51 +0300

    Bethany's pie shop

---
+++ script.js
@@ -0,0 +1,14 @@
+function save(){
+    localStorage.setItem(
+        "applepie", true
+    );
+}
+    window.onload=function(){
+        var favorite = document.getElementById('favorite');
+        if (window.localStorage){
+            var storage= window.localStorage;
+            if(storage.getItem('applepie')== 'true'){
+                favorite.innerHTML= "Apple pie is one of your favorites"
+            }
+        }
+    }
